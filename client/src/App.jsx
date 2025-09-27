87b7743af68f84ea8f0d65c614cad7095d9cad33
Mariam Hagras <mariamhagras99@gmail.com>
2025-09-27 23:41:21 +0300

    solved case sensitive file name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import Emailverify from './pages/Emailverify'
+import Emailverify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
 import { ToastContainer } from 'react-toastify';
 
