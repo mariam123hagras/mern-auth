2cccb1a10a547d1baa90e602c815fdc306e94a10
Mariam Hagras <mariamhagras99@gmail.com>
2025-09-27 23:49:48 +0300

    origin:true

--- server/server.js
+++ server/server.js
@@ -11,12 +11,12 @@ const app = express();
 const port= process.env.PORT|| 4000;
 connectDB();
 
-const allowedOrigins=['http://localhost:5173']
+// const allowedOrigins=['http://localhost:5173','https://mern-auth-doue.vercel.app']
 
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors({origin:allowedOrigins,credentials:true}))
+app.use(cors({origin:true,credentials:true}))
 
 //API Endpoints
 app.get('/',(req,res)=>{
